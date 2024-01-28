df9776afd40f306c8b28fddd64db7af3e2f99dd6
birisi atuya <tushhamphrey@gamil.com>
2024-01-28 13:19:14 +0300

    implement lazy loading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,34 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
-import Signup from "./components/Signup";
 import Login from "./components/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AccountVerification from "./components/AccountVerification";
+import { lazy, Suspense } from "react";
+const Signup = lazy(() => import("./components/Signup"));
 
 function App() {
   const { isOpen } = useSelector((state: RootState) => state.toggleSignUp);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={isOpen ? <Signup /> : <Login />} />
+        <Route
+          path="/"
+          element={
+            isOpen ? (
+              <Suspense
+                fallback={
+                  <div className="vh-100 d-flex align-items-center justify-content-center">
+                    <h6>Loading...</h6>
+                  </div>
+                }
+              >
+                <Signup />
+              </Suspense>
+            ) : (
+              <Login />
+            )
+          }
+        />
         <Route path="/verification" element={<AccountVerification />} />
       </Routes>
     </BrowserRouter>
